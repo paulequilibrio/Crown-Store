beffd0cb63de9096b256f5f30fbb5655a53824b5
Awa02 <37822765+Awa02@users.noreply.github.com>
2019-12-29 05:44:08 +0000

    added React-Router to the project and created routes for each  menu

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
+import {Route} from 'react-router-dom';
 import HomePage from "./Components/Pages/Homepages/Homepage.component";
 
 function App() {
   return (
     <div>
-    <HomePage/>
+        <Route exact path='/' component={HomePage}/>       
     </div>
   );
 }
--- src/Components/Pages/Homepages/Menu-item/Menu-item.component.jsx
+++ src/Components/Pages/Homepages/Menu-item/Menu-item.component.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 import './Menu-item-Styles.scss';
 
-const MenuItem = ({title, imageUrl, size}) => {
+const MenuItem = ({title, imageUrl, size, linkUrl, match, history}) => {
     return (
-         <div className={`${size} menu-item`}>
+         <div className={`${size} menu-item`} onClick={() => history.push(`${match.url}${linkUrl}`)}>
             <div className='background-image' style={{background: `url(${imageUrl})`}}/>
                  <div className='content'>
                      <h1 className='title'>{title.toUpperCase()}</h1>
                      <span className='subtitle'>SHOP NOW</span>
                 </div>         
             </div>
-
     )
 }
 
 
-export default MenuItem;
+export default withRouter(MenuItem);
